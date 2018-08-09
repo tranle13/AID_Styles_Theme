255cb56d8fb8f3d59c13ed21d1400a0e6b805c67
Tran Le <ttle@fullsail.edu>
2018-08-09 14:20:17 -0400

    Completed code to alter color of text and background based on radio group and change the textview based on user input

--- LeTran_CE03/app/src/main/java/com/sunny/android/letran_ce03/MainActivity.java
+++ LeTran_CE03/app/src/main/java/com/sunny/android/letran_ce03/MainActivity.java
@@ -5,14 +5,150 @@
 
 package com.sunny.android.letran_ce03;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout frameLayout = null;
+    private RadioGroup radioGroup_1 = null;
+    private  RadioGroup radioGroup_2 = null;
+    private TextView frameLayoutText = null;
+    private EditText changeText = null;
+    private Switch bold = null;
+    private static final String TAG = "hello";
+    private int chosenColor = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getViewWithOrientation();
+        radioGroup_1.setOnCheckedChangeListener(radioGroupCheckedChanged);
+        radioGroup_2.setOnCheckedChangeListener(radioGroupCheckedChanged);
+        bold.setOnCheckedChangeListener(switchChanged);
+        changeText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                changeText.setError(null);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                frameLayoutText.setText(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                changeText.setError(null);
+            }
+        });
+    }
+
+    // Create function to change frame layout background color
+    private RadioGroup.OnCheckedChangeListener radioGroupCheckedChanged = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int i) {
+            if (radioGroup.getId() == R.id.rdg_RadioGroup1) {
+                switch (i) {
+                    case R.id.rbn_BackgroundPurple:
+                        frameLayout.setBackgroundColor(getColor(R.color.purple));
+                        break;
+                    case R.id.rbn_BackgroundGreen:
+                        frameLayout.setBackgroundColor(getResources().getColor(R.color.green, null));
+                        break;
+                    case R.id.rbn_BackgroundBlack:
+                        frameLayout.setBackgroundColor(getColor(R.color.black));
+                        break;
+                    default:
+                        Log.i(TAG, "onCheckedChanged: OH GOSH");
+                        break;
+                }
+            } else {
+                switch (i) {
+                    case R.id.rbn_TextPurple:
+                        frameLayoutText.setTextAppearance(R.style.TextColor);
+                        chosenColor = 0;
+                        break;
+                    case R.id.rbn_TextGreen:
+                        frameLayoutText.setTextAppearance(R.style.TextColor_Green);
+                        chosenColor = 1;
+                        break;
+                    case R.id.rbn_TextBlack:
+                        frameLayoutText.setTextAppearance(R.style.TextColor_Black);
+                        chosenColor = 2;
+                        break;
+                    default:
+                        Log.i(TAG, "onCheckedChanged: CRASH AND BURN!");
+                        break;
+                }
+            }
+        }
+    };
+
+    private Switch.OnCheckedChangeListener switchChanged = new Switch.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+            if (b == true) {
+                switch (chosenColor) {
+                    case 0:
+                        Log.i(TAG, "onCheckedChanged: It's purple!");
+                        frameLayoutText.setTextAppearance(R.style.TextColor_Bold);
+                        break;
+                    case 1:
+                        frameLayoutText.setTextAppearance(R.style.TextColor_Green_Bold);
+                        break;
+                    case 2:
+                        frameLayoutText.setTextAppearance(R.style.TextColor_Black_Bold);
+                        break;
+                    default:
+                        Log.i(TAG, "onCheckedChanged: OH MY GOSH");
+                        break;
+                }
+            } else {
+                switch (chosenColor) {
+                    case 0:
+                        frameLayoutText.setTextAppearance(R.style.TextColor_Normal);
+                        break;
+                    case 1:
+                        frameLayoutText.setTextAppearance(R.style.TextColor_Green_Normal);
+                        break;
+                    case 2:
+                        frameLayoutText.setTextAppearance(R.style.TextColor_Black_Normal);
+                        break;
+                    default:
+                        Log.i(TAG, "onCheckedChanged: OH HELLO GOSH");
+                        break;
+                }
+            }
+        }
+    };
+
+    private void getViewWithOrientation() {
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            frameLayout = (FrameLayout)findViewById(R.id.frameLayout_Portrait);
+            radioGroup_1 = (RadioGroup)findViewById(R.id.rdg_RadioGroup1);
+            radioGroup_2 = (RadioGroup)findViewById(R.id.rdg_RadioGroup2);
+            frameLayoutText = (TextView)findViewById(R.id.txt_FrameLayoutText);
+            changeText = (EditText)findViewById(R.id.txt_ChangeText);
+            bold = (Switch)findViewById(R.id.swt_Bold);
+        } else {
+            frameLayout = (FrameLayout)findViewById(R.id.frameLayout_Land);
+            radioGroup_1 = (RadioGroup)findViewById(R.id.rdg_RadioGroup1_Land);
+            radioGroup_2 = (RadioGroup)findViewById(R.id.rdg_RadioGroup2_Land);
+            frameLayoutText = (TextView)findViewById(R.id.txt_FrameLayoutText_Land);
+            changeText = (EditText)findViewById(R.id.txt_ChangeText_Land);
+            bold = (Switch)findViewById(R.id.swt_Bold_Land);
+        }
     }
 }
