5de3eba3d58124463a7dfd2d736083b7b407d933
Tran Le <ttle@fullsail.edu>
2018-08-12 18:40:45 -0400

    Optimized code and added signed APK

--- LeTran_CE03/app/src/main/java/com/sunny/android/letran_ce03/MainActivity.java
+++ LeTran_CE03/app/src/main/java/com/sunny/android/letran_ce03/MainActivity.java
@@ -5,7 +5,6 @@
 
 package com.sunny.android.letran_ce03;
 
-import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -34,11 +33,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getViewWithOrientation();
+        getView();
+
         radioGroup_1.setOnCheckedChangeListener(radioGroupCheckedChanged);
         radioGroup_2.setOnCheckedChangeListener(radioGroupCheckedChanged);
         bold.setOnCheckedChangeListener(switchChanged);
 
+        // Function to listen if the text in edittext changes
         changeText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
@@ -58,22 +59,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Create function to change frame layout background color
-    private RadioGroup.OnCheckedChangeListener radioGroupCheckedChanged = new RadioGroup.OnCheckedChangeListener() {
+    private final RadioGroup.OnCheckedChangeListener radioGroupCheckedChanged = new RadioGroup.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup radioGroup, int i) {
-            if (radioGroup.getId() == R.id.rdg_RadioGroup1 || radioGroup.getId() == R.id.rdg_RadioGroup1_Land) {
-                if (i == R.id.rbn_BackgroundPurple || i == R.id.rbn_BackgroundPurple_Land) {
+            if (radioGroup.getId() == R.id.rdg_RadioGroup1) {
+                if (i == R.id.rbn_BackgroundPurple) {
                     frameLayout.setBackgroundColor(getColor(R.color.purple));
-                } else if (i == R.id.rbn_BackgroundGreen || i == R.id.rbn_BackgroundGreen_Land) {
+                } else if (i == R.id.rbn_BackgroundGreen) {
                     frameLayout.setBackgroundColor(getColor(R.color.green));
                 } else {
                     frameLayout.setBackgroundColor(getColor(R.color.black));
                 }
             } else {
-                if (i == R.id.rbn_TextPurple || i == R.id.rbn_TextPurple_Land) {
+                if (i == R.id.rbn_TextPurple) {
                     frameLayoutText.setTextAppearance(R.style.TextColor);
                     chosenColor = 0;
-                } else if (i == R.id.rbn_TextGreen || i == R.id.rbn_TextGreen_Land) {
+                } else if (i == R.id.rbn_TextGreen) {
                     frameLayoutText.setTextAppearance(R.style.TextColor_Green);
                     chosenColor = 1;
                 } else {
@@ -84,13 +85,13 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private Switch.OnCheckedChangeListener switchChanged = new Switch.OnCheckedChangeListener() {
+    // Function to check if the bold switch changes
+    private final Switch.OnCheckedChangeListener switchChanged = new Switch.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-            if (b == true) {
+            if (b) {
                 switch (chosenColor) {
                     case 0:
-                        Log.i(TAG, "onCheckedChanged: It's purple!");
                         frameLayoutText.setTextAppearance(R.style.TextColor_Bold);
                         break;
                     case 1:
@@ -123,28 +124,13 @@ public class MainActivity extends AppCompatActivity {
     };
 
     // Create function to get view depending on orientation
-    private void getViewWithOrientation() {
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            frameLayout = (FrameLayout) findViewById(R.id.frameLayout_Portrait);
-            radioGroup_1 = (RadioGroup) findViewById(R.id.rdg_RadioGroup1);
-            radioGroup_2 = (RadioGroup) findViewById(R.id.rdg_RadioGroup2);
-            frameLayoutText = (TextView) findViewById(R.id.txt_FrameLayoutText);
-            changeText = (EditText) findViewById(R.id.txt_ChangeText);
-            bold = (Switch) findViewById(R.id.swt_Bold);
-        } else {
-            frameLayout = (FrameLayout) findViewById(R.id.frameLayout_Land);
-            radioGroup_1 = (RadioGroup) findViewById(R.id.rdg_RadioGroup1_Land);
-            radioGroup_2 = (RadioGroup) findViewById(R.id.rdg_RadioGroup2_Land);
-            frameLayoutText = (TextView) findViewById(R.id.txt_FrameLayoutText_Land);
-            changeText = (EditText) findViewById(R.id.txt_ChangeText_Land);
-            bold = (Switch) findViewById(R.id.swt_Bold_Land);
-        }
+    private void getView() {
+        frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
+        radioGroup_1 = (RadioGroup) findViewById(R.id.rdg_RadioGroup1);
+        radioGroup_2 = (RadioGroup) findViewById(R.id.rdg_RadioGroup2);
+        frameLayoutText = (TextView) findViewById(R.id.txt_FrameLayoutText);
+        changeText = (EditText) findViewById(R.id.txt_ChangeText);
+        bold = (Switch) findViewById(R.id.swt_Bold);
     }
 
-    // Function to get view whenever orientation changes
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        getViewWithOrientation();
-    }
 }
